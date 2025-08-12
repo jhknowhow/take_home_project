70bf450cf53403930cc0b68ca8101cc9e2c44d17
PARK\trap0 <jhknowhow@gmail.com>
2025-08-12 20:46:19 +0900

    playwright 프로젝트 생성

---
+++ tests/example.spec.js
@@ -0,0 +1,24 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  console.log('Navigated to https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+  console.log('Title contains "Playwright"');
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  console.log('Navigated to https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+  console.log('Clicked "Get started" link');
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  console.log('Verified "Installation" heading is visible');
+});
